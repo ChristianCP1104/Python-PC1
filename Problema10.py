9ded24ee7ef70e04c8fce15747720a10d4341cbb
ChristianCP1104 <christian.costilla@unmsm.edu.pe>
2024-07-14 04:55:24 +0000

    subiendo problema10

--- Problema10.py
+++ Problema10.py
@@ -1,5 +1,6 @@
 lista_muestra = ['Rojo','Verde','Blanco','Negro','Rosa','Amarillo']
-lista_muestra.remove('Rojo')
-lista_muestra.remove('Rosa')
-lista_muestra.remove('Amarillo')
+elementos_eliminar=[0,4,5]
+nueva_lista=[lista_muestra[i] for i in elementos_eliminar]
+for valor in nueva_lista:
+    lista_muestra.remove(valor)
 print(lista_muestra)
