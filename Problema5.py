4c240eb38e90fc3ed6c9a65d626823071a86c8da
ChristianCP1104 <christian.costilla@unmsm.edu.pe>
2024-07-14 04:10:58 +0000

    subiendo problema5

---
+++ Problema5.py
@@ -0,0 +1,6 @@
+numero_shows = int(input("Ingrese el número de shows musicales que ha visto en el último año: "))
+
+# Condicion: el usuario ha visto más de 3 shows
+visto = numero_shows > 3
+
+print(visto)
